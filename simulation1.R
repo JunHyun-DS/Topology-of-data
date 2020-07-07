10fbd59e86aa011407b932f8e3caabb0f3052ee2
JunHyun-DS <60165706+JunHyun-DS@users.noreply.github.com>
2020-07-07 23:34:18 +0900

    Update simulation1.R

--- simulation1.R
+++ simulation1.R
@@ -1,23 +1,24 @@
 ## 4개의 정규분포
-
-set.seed(1000)
-df=runif(1000,-1,1)
-
-x1 = rnorm(df,mean=5,sd=1)
-y1 = rnorm(df, mean=5, sd=1)
-
-x2 = rnorm(df, mean=5, sd=1)
-y2 = rnorm(df, mean=-5, sd=1)
-
-x3 = rnorm(df, mean=-5, sd=1)
-y3 = rnorm(df, mean=-5, sd=1)
-
-x4 = rnorm(df, mean=-5, sd=1)
-y4 = rnorm(df, mean=5, sd=1)
-
-x = c(x1,x2,x3,x4)
-y = c(y1,y2,y3,y4)
-
-simulated_df = data.frame(x,y)
-
-plot(x,y)
+simulation_data = function(){
+  set.seed(1000)
+  df=runif(1000,-1,1)
+  
+  x1 = rnorm(df,mean=5,sd=1)
+  y1 = rnorm(df, mean=5, sd=1)
+  
+  x2 = rnorm(df, mean=5, sd=1)
+  y2 = rnorm(df, mean=-5, sd=1)
+  
+  x3 = rnorm(df, mean=-5, sd=1)
+  y3 = rnorm(df, mean=-5, sd=1)
+  
+  x4 = rnorm(df, mean=-5, sd=1)
+  y4 = rnorm(df, mean=5, sd=1)
+  
+  x = c(x1,x2,x3,x4)
+  y = c(y1,y2,y3,y4)
+  
+  simulation_data = data.frame(x,y)
+  
+  return(simulation_data) 
+}
